5346c215f041094aafbc7bb1b0c29c8931ed2375
dovanchien123 <congtodvc@gmail.com>
2020-01-11 01:43:28 +0700

    chienck11

--- router/witRouter.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const witRouter = express.Router();
-const getEntities = require("../witapi/getEntities");
-witRouter.post("/getEntities", getEntities);
-
-module.exports = witRouter;
